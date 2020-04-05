56b87870561f88e0b0dc4ffafcafb481f71eb05d
iyear <ljyngup@gmail.com>
2020-04-05 21:01:26 +0800

    增加获取最近log

--- util.go
+++ util.go
@@ -4,13 +4,21 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/url"
 	"os"
 	"strings"
+	"time"
 )
 
 //true=>no error
+func Min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
 func CheckErr(err error) bool {
 	if err != nil {
 		log.Println(err)
@@ -62,4 +70,43 @@ func GetMD5Encode(data string) string {
 //返回一个16位md5加密后的字符串
 func Get16MD5Encode(data string) string {
 	return GetMD5Encode(data)[8:24]
+}
+
+//只返回文件名
+func GetPathFiles(path string) []string {
+	files, _ := ioutil.ReadDir(path)
+	var t []string
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		} else {
+			t = append(t, file.Name())
+		}
+	}
+	return t
+}
+
+//输入文件夹路径，返回最近n个log的路径，不到n个返回所有
+func GetRecentLogs(path string, n int) []string {
+	var paths []string
+	if !PathExists(path) {
+		return paths
+	}
+	//path末尾检查/
+	if path[len(path)-1:] != "/" {
+		path += "/"
+	}
+	data := time.Now()
+	d, _ := time.ParseDuration("-24h")
+	//不到n个返回所有
+	nt := Min(n, len(GetPathFiles(path)))
+	//fmt.Println(nt)
+	for i := 1; i <= nt; {
+		if PathExists(path + data.Format("2006-01-02") + ".log") {
+			paths = append(paths, path+data.Format("2006-01-02")+".log")
+			i++
+		}
+		data = data.Add(d)
+	}
+	return paths
 }
