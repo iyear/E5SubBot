ec0421000f44f51bb95f1e9ddd2f5d007667253b
iyear <ljyngup@gmail.com>
2020-04-09 15:54:19 +0800

    Update

--- control.go
+++ control.go
@@ -173,11 +173,11 @@ func GetAdmin() []int64 {
 	return result
 }
 func InitLogger() {
-	if !PathExists("./log/") {
-		os.Mkdir("./log/", 0773)
+	if !PathExists(bLogBasePath) {
+		os.Mkdir(bLogBasePath, 0773)
 	}
 
-	path := "./log/" + time.Now().Format("2006-01-02") + ".log"
+	path := bLogBasePath + time.Now().Format("2006-01-02") + ".log"
 	f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0773)
 	if err != nil {
 		logger.Println(err)
