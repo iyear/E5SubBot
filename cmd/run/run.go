01d92b905d3813f3edef2e525a8e7f74f3710947
iyear <ljyngup@gmail.com>
2022-04-19 21:29:22 +0800

    :bug: fix `run` cmd flags

--- cmd/run/run.go
+++ cmd/run/run.go
@@ -13,15 +13,15 @@ var (
 
 var Cmd = &cobra.Command{
 	Use:   "run",
-	Short: "Start the local server",
-	Long:  `Start the local server`,
+	Short: "Start the bot",
+	Long:  `Start the bot`,
 	Run: func(cmd *cobra.Command, args []string) {
 		bot.Run(cfg, tmplPath, dataPath)
 	},
 }
 
 func init() {
-	Cmd.PersistentFlags().StringVarP(&cfg, "config", "c", "config/config.yaml", "bot config file")
+	Cmd.PersistentFlags().StringVarP(&cfg, "config", "c", "config.yaml", "bot config file")
 	Cmd.PersistentFlags().StringVarP(&tmplPath, "template", "t", "./template", "template files path")
 	Cmd.PersistentFlags().StringVarP(&dataPath, "data", "d", "./data", "data files path")
 }
