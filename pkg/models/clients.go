e7766b79316549bebc7a39501a2bacafc3f6ca8b
iyear <ljyngup@gmail.com>
2022-04-26 21:34:02 +0800

    :zap: add indexes

--- pkg/models/clients.go
+++ pkg/models/clients.go
@@ -1,11 +1,11 @@
 package models
 
 type Client struct {
-	ID           int64  `gorm:"primaryKey;autoIncrement;not null"`
-	TgId         int64  `gorm:"not null"`
+	ID           int64  `gorm:"primaryKey;autoIncrement;not null;index"`
+	TgId         int64  `gorm:"not null;index"`
 	RefreshToken string `gorm:"not null"`
 	MsId         string `gorm:"not null"`
-	Uptime       int64  `gorm:"autoUpdateTime;not null"`
+	Uptime       int64  `gorm:"autoUpdateTime;not null;index"`
 	Alias        string `gorm:"not null"`
 	ClientId     string `gorm:"not null"`
 	ClientSecret string `gorm:"not null"`
